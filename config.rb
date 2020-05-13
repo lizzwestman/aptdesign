70a10507bf32dfa5f6da1bdbbf077330ee1f520e
Lizz Westman <4702622+lizzwestman@users.noreply.github.com>
2020-05-13 16:37:54 -0700

    Add files via upload

---
+++ config.rb
@@ -0,0 +1,25 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "/Users/lizz/Documents/_clients2020/APTDESIGN/html"
+css_dir = "css"
+sass_dir = ""
+images_dir = "images"
+generated_images_path = "/Users/lizz/Documents/_clients2020/APTDESIGN/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :expanded
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass  scss && rm -rf sass && mv scss sass
+sourcemap = true
